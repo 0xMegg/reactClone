a8c6cb7918802fca5222cbef0c3fc5e6c148b5c1
Megg <finero91@gmail.com>
2025-07-07 15:59:35 +0900

    until 0707 16:00

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import React from "react";
 import "./App.css";
-import { Gift, Search, ShoppingBag, User } from "lucide-react";
+import { ArrowRight, Gift, Search, ShoppingBag, User } from "lucide-react";
 
 function App() {
+  const getUrlArr = (n: number): string[] => {
+    const newArr = [];
+    for (let i = 0; i < n; i++) {
+      newArr[i] = `https://picsum.photos/600?random=${i}`;
+    }
+    return newArr;
+  };
+
+  const urlArr = getUrlArr(30);
+
+  const getGrid = (n: number): React.ReactElement => {
+    return (
+      <div className="grid">
+        {Array.from({ length: n }).map((_, i) => (
+          <img key={i} src={urlArr[i + 3]} alt="" />
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div className="page">
-      <div>
+      {/* 헤더 */}
+      <div className="fixed">
         <header>
           {/* 왼쪽 */}
           <div className="left">
@@ -56,18 +77,49 @@ function App() {
           </div>
         </header>
         <nav>
-          <button>지금핫한</button>
-          <button>실시간 랭킹</button>
-          <button>오늘신작</button>
-          <button>이벤트</button>
-          <button>남성인기</button>
-          <button>완결추천</button>
-          <button>완결판</button>
-          <button>여성인기</button>
-          <button>TV속 작품</button>
-          <button>브랜드</button>
+          <div className="buttons">
+            <button>지금핫한</button>
+            <button>실시간 랭킹</button>
+            <button>오늘신작</button>
+            <button>이벤트</button>
+            <button>남성인기</button>
+            <button>완결추천</button>
+            <button>완결판</button>
+            <button>여성인기</button>
+            <button>TV속 작품</button>
+            <button>브랜드</button>
+          </div>
         </nav>
       </div>
+      {/* 배너 */}
+      <div className="contents">
+        <div className="banner">
+          <div className="left">
+            <img src="../../public/img.jpg" alt="" />
+            <p>#럭키위크 #마지막 캐시 선물!</p>
+          </div>
+          <button className="right">
+            <ArrowRight />
+          </button>
+        </div>
+      </div>
+      {/* 1층 */}
+      <div className="first">
+        {/* left */}
+        <div className="left">
+          <img src={`${urlArr[0]}`} alt="" />
+        </div>
+        {/* mid */}
+        <div className="mid">
+          <img src={`${urlArr[1]}`} alt="" />
+        </div>
+        {/* right */}
+        <div className="right">
+          <img src={`${urlArr[2]}`} alt="" />
+        </div>
+      </div>
+      {/* 2층 */}
+      <div>{getGrid(6)}</div>
     </div>
   );
 }
